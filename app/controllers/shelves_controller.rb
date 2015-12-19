77fed7d4cd669adb20b94289c9da8a4244ee020f
Juin Chiu <davidjuin0519@gmail.com>
2015-12-19 19:17:54 +0800

    add content_for helper for header & footer

--- app/controllers/shelves_controller.rb
+++ app/controllers/shelves_controller.rb
@@ -2,4 +2,8 @@ class ShelvesController < ApplicationController
   def index
     @shelves =@paginate = Shelf.all.paginate(:page => params[:page])
   end
+
+  def show
+    @shelf = Shelf.find(params[:id])
+  end
 end
