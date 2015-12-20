a08133384b543c996ba39eb95c4a221d7ce31aa9
Juin Chiu <davidjuin0519@gmail.com>
2015-12-20 21:20:25 +0800

    implement template on show of shelf

--- app/controllers/shelves_controller.rb
+++ app/controllers/shelves_controller.rb
@@ -1,6 +1,6 @@
 class ShelvesController < ApplicationController
   def index
-    @shelves = @paginate = Shelf.all.paginate(:page => params[:page])
+    @shelves = @paginate = Shelf.all.order('id DESC').paginate(:page => params[:page])
   end
 
   def show
