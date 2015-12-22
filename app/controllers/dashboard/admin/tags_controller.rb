bbdb25939e4fff925067da142e5e08213074444f
Juin Chiu <davidjuin0519@gmail.com>
2015-12-22 17:07:40 +0800

    complete tags CRUD for admin

--- app/controllers/dashboard/admin/tags_controller.rb
+++ app/controllers/dashboard/admin/tags_controller.rb
@@ -1,2 +1,40 @@
 class Dashboard::Admin::TagsController < Dashboard::Admin::AdminController
+  def index
+    @tags = @paginate = Tag.all.order('id DESC').paginate(:page => params[:page])
+  end
+
+  def new
+    @tag = Tag.new
+  end
+
+  def edit
+    @tag = Tag.find(params[:id])
+  end
+
+  def create
+    @tag = Tag.new(tag_params)
+    @tag.save
+    redirect_to dashboard_admin_tags_path
+  end
+
+  def update
+    @tag = Tag.find(params[:id])
+
+    if @tag.update(tag_params)
+      redirect_to dashboard_admin_tags_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @tag = Tag.find(params[:id])
+    @tag.destroy
+    redirect_to dashboard_admin_tags_path
+  end
+
+  private
+  def tag_params
+    params.require(:tag).permit(:name)
+  end
 end
