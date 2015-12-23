59ed5bdb1c01dc7ac0037f7a9776102e05ff1876
Juin Chiu <davidjuin0519@gmail.com>
2015-12-23 11:01:02 +0800

    add nav-pills on shelves index

--- app/controllers/shelves_controller.rb
+++ app/controllers/shelves_controller.rb
@@ -1,5 +1,6 @@
 class ShelvesController < ApplicationController
   def index
+    @tag_id = Tag.find(params[:tag]).id if params[:tag]
     @shelves = @paginate = Shelf.all.order('id DESC').paginate(:page => params[:page])
   end
 
