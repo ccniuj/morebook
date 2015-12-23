afaf6a8ce3e0347dda83f0bd547afcf91a429cac
Juin Chiu <davidjuin0519@gmail.com>
2015-12-23 18:02:12 +0800

    homepage random recommendation

--- app/controllers/statics_controller.rb
+++ app/controllers/statics_controller.rb
@@ -1,5 +1,6 @@
 class StaticsController < ApplicationController
   def index
-    @books = @paginate = Book.paginate(:page => params[:page])
+    # @books = @paginate = Book.paginate(:page => params[:page])
+    @shelves = Shelf.offset(rand(Shelf.count)).limit(8)
   end
 end
