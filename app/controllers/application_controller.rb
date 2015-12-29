cf7c28025ccdeccbe8abefa80882e7d98154923f
Juin Chiu <davidjuin0519@gmail.com>
2015-12-29 16:14:03 +0800

    put selfie on nav of browsing page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,27 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
-
+  before_action :create_profile
+  @@selfie_default_url = 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTMQRG8v0aaUxcbIIBX9rAlnIykCgxzHLn3t_zILkWwt-kOYaP80z0t-ZQ'
+  
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) do |u| 
       u.permit(:name, :email, :password, :password_confirmation, :remember_me) 
+    end
+  end
+
+  private
+
+  def create_profile
+    if current_user
+      current_user.profile ||= Profile.create(
+        :user_id => current_user.id,
+        :name    => current_user.name,
+        :email   => current_user.email,
+        :selfie  => @@selfie_default_url
+        )
     end
   end
 end
--- app/controllers/dashboard/dashboard_controller.rb
+++ app/controllers/dashboard/dashboard_controller.rb
@@ -1,18 +1,4 @@
 class Dashboard::DashboardController < ApplicationController
   before_action :authenticate_user!
-  before_action :create_profile
   layout 'dashboard'
-  
-  @@selfie_default_url = 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTMQRG8v0aaUxcbIIBX9rAlnIykCgxzHLn3t_zILkWwt-kOYaP80z0t-ZQ'
-  
-  private
-
-  def create_profile
-    current_user.profile ||= Profile.create(
-      :user_id => current_user.id,
-      :name    => current_user.name,
-      :email   => current_user.email,
-      :selfie  => @@selfie_default_url
-      )
-  end
 end
