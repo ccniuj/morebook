09ac7ed05b0056bf82cb72de1e78062abdd0babc
Juin Chiu <davidjuin0519@gmail.com>
2016-01-21 15:59:25 +0800

    fix create profile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,6 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :create_profile
 
-  @@selfie_default_url = 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTMQRG8v0aaUxcbIIBX9rAlnIykCgxzHLn3t_zILkWwt-kOYaP80z0t-ZQ'
   WillPaginate.per_page = 40
 
   protected
@@ -24,7 +23,6 @@ class ApplicationController < ActionController::Base
         :user_id => current_user.id,
         :name    => current_user.name,
         :email   => current_user.email,
-        :selfie  => @@selfie_default_url
         )
     end
   end
